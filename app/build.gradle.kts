ae825cc5fbcfa58502b614743c1fb0ba137548c8
Jason Ernst <ernstjason1@gmail.com>
2024-11-01 08:42:12 -0700

    upgrade to sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.jasonernst.stream_telemetry"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.jasonernst.stream_telemetry"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
